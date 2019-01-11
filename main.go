0b6a1b5c3b30071aa4588fb3be95583e38d8c14a
andresr <andresr@clientmaker.co>
2019-01-11 21:56:30 +0000

    Docker bash

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func Sum(a, b int) int {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(fmt.Sprintf("%d", Sum(251123, 42))))
+	w.Write([]byte(fmt.Sprintf("%d", Sum(8, 42))))
 }
 
 func main() {
